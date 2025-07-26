cfd4581cc01f9441bf91f38779ed65ac4dff68fb
Max Sotnikov <maxsotnikov00@gmail.com>
2025-07-27 00:42:23 +0300

    lesson-02 attempt 2

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,10 +13,10 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess = true
+let isAccess = ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass))
 
 // your code
-if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
-    console.log(isAccess);
-}
+// if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
+//     console.log(isAccess);
+// }
 
