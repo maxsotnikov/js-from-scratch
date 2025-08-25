9c9a648a0225a4dac8e7e5a58d4a3f8b1df77b2f
Max Sotnikov <maxsotnikov00@gmail.com>
2025-08-25 19:13:43 +0300

    lesson-06 attempt 1

---
+++ lesson-06/01.js
@@ -0,0 +1,38 @@
+/*
+Напишите свою версию метода `filter`.
+Ваша задача — создать функцию `filter`, которая принимает два параметра:
+
+- `array` (массив, над которым выполняется операция),
+- `callback` (функция, которая вызывается для каждого элемента массива).
+
+Функция `callback` должна принимать два параметра:
+
+- `element` (текущий элемент массива)
+- `index` (индекс текущего элемента)
+
+Функция `filter` должна возвращать новый массив, содержащий только те элементы `array`, для которых `callback` возвращает `true`.
+*/
+
+/* Пример использования:
+const numbers = [1, 2, 3, 4, 5]
+
+const oddNumbers = filter(numbers, (element, index) => {
+  return element % 2 !== 0
+});
+
+console.log(oddNumbers) // Должен вывести: [1, 3, 5]
+*/
+
+const filter = (array, callback) => {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const elem = array[i];
+    if (callback(elem, i)) {
+      newArray.push(elem);
+    }
+  }
+
+  return newArray;
+};
+
+
